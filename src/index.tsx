5971a0b8d8c365f7ea165d14768ce5650aaf3451
Josh Kaplan <josh-kaplan@users.noreply.github.com>
2024-05-01 14:28:21 -0400

    Update auth redirect

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const oidcConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_bPesfcUZG",
   //authority: "https://trg.auth.us-east-1.amazoncognito.com/trg-software-users",
   client_id: "4rts2aoe8mf0mfgapqcd3i8dc2", // trg-web-ui
-  redirect_uri: "http://localhost:3000",
+  redirect_uri: "https://trgsoftware.net", // use http://localhost:3000 for local development
   responseType: 'code',
   scope: 'openid profile'
 };
